c4e77031f8d1e166d02541b4fbbfab73055b71dc
Felix Dango <felix.dango@reaze.com>
2021-08-23 11:58:29 +0200

    add model SafetyBriefing
    closes #6

--- print/models.py
+++ print/models.py
@@ -1,16 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Print(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    datetime = models.DateField()
-
-class Printer(models.Model):
-    print = models.ForeignKey(Print, on_delete=models.CASCADE)
-    printing = models.BooleanField()
-
-class Rating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    print = models.ForeignKey(Print, on_delete=models.CASCADE)
-    comment = models.TextField(max_length=500)
-    rating = models.IntegerField()
+class SafetyBriefing(models.Model):
+    kind = models.TextField(max_length=500)
+    validity_period = models.IntegerField()
+    document = models.BinaryField()
