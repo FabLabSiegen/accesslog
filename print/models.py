dc860fe1ab89cb342ffbc0cc0585523415eecf38
Felix Dango <felix.dango@reaze.com>
2021-08-23 12:11:51 +0200

    add model FabLabUser and make many to many relation to SafetyBriefing
    resolves #7

--- print/models.py
+++ print/models.py
@@ -5,3 +5,10 @@ class SafetyBriefing(models.Model):
     kind = models.TextField(max_length=500)
     validity_period = models.IntegerField()
     document = models.BinaryField()
+
+class FabLabUser(models.Model):
+    name = models.TextField(max_length=100)
+    email = models.TextField(max_length=200)
+    registered = models.DateTimeField()
+    rfid_uuid = models.IntegerField()
+    safety_briefings = models.ManyToManyField(SafetyBriefing)
