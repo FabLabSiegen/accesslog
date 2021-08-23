cb98a529cb6f4e3c5d290c399aab9f378107d35a
Felix Dango <felix.dango@reaze.com>
2021-08-23 13:59:43 +0200

    fix: change some error with old migrations Follow steps to resolve local errors: 1. Delete your migrations folder 2. In the database: DELETE FROM django_migrations WHERE app = 'app_name'.    You could alternatively just truncate this table. 3. python manage.py makemigrations app_name 4. python manage.py migrate

--- print/models.py
+++ print/models.py
@@ -9,6 +9,15 @@ class SafetyBriefing(models.Model):
 class FabLabUser(models.Model):
     name = models.TextField(max_length=100)
     email = models.TextField(max_length=200)
-    registered = models.DateTimeField()
     rfid_uuid = models.IntegerField()
-    safety_briefings = models.ManyToManyField(SafetyBriefing)
+    can_brief = models.BooleanField()
+    safety_briefings = models.ManyToManyField(SafetyBriefing, through='UserIsBriefed',)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class UserIsBriefed(models.Model):
+    recipient = models.ForeignKey(FabLabUser, on_delete=models.CASCADE)
+    safety_briefings = models.ForeignKey(SafetyBriefing, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    instructor_id = models.IntegerField()
+
+
