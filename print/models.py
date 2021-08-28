7bd7fabd2b25037f6e62be74fb1ef7fd2ad5bdbc
Felix Dango <felix.dango@reaze.com>
2021-08-28 14:33:25 +0200

    fix: change naming to PascalCase

--- print/models.py
+++ print/models.py
@@ -2,84 +2,83 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class SafetyBriefing(models.Model):
-    kind = models.TextField(max_length=500)
-    validity_period = models.IntegerField()
-    document = models.FileField()
+    Kind = models.TextField(max_length=500)
+    ValidityPeriod = models.IntegerField()
+    Document = models.FileField()
 
 class FabLabUser(models.Model):
-    name = models.TextField(max_length=100)
-    email = models.EmailField(max_length=200)
-    rfid_uuid = models.IntegerField()
-    can_brief = models.BooleanField()
-    safety_briefings = models.ManyToManyField(SafetyBriefing, through='UserIsBriefed', through_fields=('recipient','safety_briefing'))
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    Name = models.TextField(max_length=100)
+    Email = models.EmailField(max_length=200)
+    RfidUuid = models.IntegerField()
+    CanBrief = models.BooleanField()
+    SafetyBriefings = models.ManyToManyField(SafetyBriefing, through='UserIsBriefed', through_fields=('Recipient','SafetyBriefing'))
+    User = models.OneToOneField(User, on_delete=models.CASCADE)
 
 class UserIsBriefed(models.Model):
-    recipient = models.ForeignKey(FabLabUser, related_name='recipient', on_delete=models.CASCADE)
-    safety_briefing = models.ForeignKey(SafetyBriefing, on_delete=models.CASCADE)
-    date = models.DateTimeField()
-    instructor = models.ForeignKey(FabLabUser, related_name='instructor', on_delete=models.CASCADE, default=None)
+    Recipient = models.ForeignKey(FabLabUser, related_name='Recipient', on_delete=models.CASCADE)
+    SafetyBriefing = models.ForeignKey(SafetyBriefing, on_delete=models.CASCADE)
+    Date = models.DateTimeField()
+    Instructor = models.ForeignKey(FabLabUser, related_name='Instructor', on_delete=models.CASCADE, default=None)
 
 class MachineCategory(models.Model):
-    name = models.TextField()
+    Name = models.TextField()
 
 class Machine(models.Model):
-    category = models.ForeignKey(MachineCategory, on_delete=models.SET_NULL, null=True)
-    status = models.TextField()
-    name = models.TextField()
-    host_name = models.TextField()
-    location = models.TextField()
-    description = models.TextField()
-    user = models.ManyToManyField(FabLabUser, through='AssignedUsers')
+    Category = models.ForeignKey(MachineCategory, on_delete=models.SET_NULL, null=True)
+    Status = models.TextField()
+    Name = models.TextField()
+    HostName = models.TextField()
+    Location = models.TextField()
+    Description = models.TextField()
+    User = models.ManyToManyField(FabLabUser, through='AssignedUsers')
 
 class AssignedUsers(models.Model):
-    user = models.ForeignKey(FabLabUser, on_delete=models.CASCADE)
-    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    User = models.ForeignKey(FabLabUser, on_delete=models.CASCADE)
+    Machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
 
-class Model(models.Model):
-    file = models.FilePathField()
-    owner = models.ForeignKey(FabLabUser, on_delete=models.SET_NULL, null=True, related_name='owner')
-    uploaded = models.DateTimeField()
-    previous = models.ForeignKey("self", on_delete=models.SET_NULL, default=None, null=True)
-    shared_by_user = models.ForeignKey(FabLabUser, on_delete=models.SET_NULL, null=True, related_name='shared_by_user')
+class ThreeDimensionalModel(models.Model):
+    File = models.FilePathField()
+    Owner = models.ForeignKey(FabLabUser, on_delete=models.SET_NULL, null=True, related_name='Owner')
+    Uploaded = models.DateTimeField()
+    Previous = models.ForeignKey("self", on_delete=models.SET_NULL, default=None, null=True)
+    SharedWithUser = models.ForeignKey(FabLabUser, on_delete=models.SET_NULL, null=True, related_name='SharedWithUser')
 
 class GCode(models.Model):
-    model = models.ForeignKey(Model, on_delete=models.CASCADE)
-    file_location = models.FilePathField()
-    estimated_printing_time = models.TimeField()
-    used_filament_in_g = models.FloatField()
-    used_filament_in_mm = models.FloatField()
-    uploaded = models.DateTimeField()
-    shared_by_user = models.ForeignKey(FabLabUser, on_delete=models.SET_NULL, null=True)
+    ThreeDimensionalModel = models.ForeignKey(ThreeDimensionalModel, on_delete=models.SET_NULL, null=True, default=None)
+    FileLocation = models.FilePathField()
+    EstimatedPrintingTime = models.TimeField()
+    UsedFilamentInG = models.FloatField()
+    UsedFilamentInMm = models.FloatField()
+    Uploaded = models.DateTimeField()
+    SharedWithUser = models.ForeignKey(FabLabUser, on_delete=models.SET_NULL, null=True)
 
 class PrintJob(models.Model):
-    user = models.ForeignKey(FabLabUser, on_delete=models.SET_NULL, null=True)
-    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
-    g_code = models.ForeignKey(GCode, on_delete=models.SET_NULL, null=True)
-    start = models.DateTimeField()
-    end = models.DateTimeField()
-    state = models.IntegerField()
+    User = models.ForeignKey(FabLabUser, on_delete=models.CASCADE)
+    Machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    GCode = models.ForeignKey(GCode, on_delete=models.CASCADE)
+    Start = models.DateTimeField()
+    End = models.DateTimeField()
+    State = models.IntegerField()
 
 class PrintTemperatureHistory(models.Model):
-    print_job = models.ForeignKey(PrintJob, on_delete=models.CASCADE)
-    tool_target = models.FloatField()
-    tool_actual = models.FloatField()
-    bed_target = models.FloatField()
-    bed_actual = models.FloatField()
-    timestamp = models.DateTimeField()
+    PrintJob = models.ForeignKey(PrintJob, on_delete=models.CASCADE)
+    ToolTarget = models.FloatField()
+    ToolActual = models.FloatField()
+    BedTarget = models.FloatField()
+    BedActual = models.FloatField()
+    TimeStamp = models.DateTimeField()
 
 class PrintMediaFile(models.Model):
-    print_job = models.ForeignKey(PrintJob, on_delete=models.CASCADE)
-    file_location = models.FilePathField()
-    description = models.TextField()
+    PrintJob = models.ForeignKey(PrintJob, on_delete=models.CASCADE)
+    FileLocation = models.FilePathField()
+    Description = models.TextField()
 
 class SlicingConfig(models.Model):
-    g_code = models.ForeignKey(GCode,on_delete=models.CASCADE)
-    parameter_1 = models.IntegerField()
-    parameter_n = models.IntegerField()
+    GCode = models.ForeignKey(GCode,on_delete=models.CASCADE)
+    ConfigLocation = models.FilePathField()
 
 class Rating(models.Model):
-    user = models.ForeignKey(FabLabUser, on_delete=models.CASCADE)
-    print_job = models.ForeignKey(PrintJob, on_delete=models.CASCADE)
-    comment = models.TextField(max_length=500)
-    rating = models.IntegerField()
+    User = models.ForeignKey(FabLabUser, on_delete=models.CASCADE)
+    PrintJob = models.ForeignKey(PrintJob, on_delete=models.CASCADE)
+    Comment = models.TextField(max_length=500)
+    Rating = models.IntegerField()
