da2a6773036ae603b6e0d2ad7416fcd6174a4d54
Felix Dango <felix.dango@reaze.com>
2021-08-31 14:40:57 +0200

    initial environment setup for rest connections

--- rest/views.py
+++ rest/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest.serializers import UserSerializer, GroupSerializer
+from print import *
 
 
 class UserViewSet(viewsets.ModelViewSet):
