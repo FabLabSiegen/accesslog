a66ffb7111e2a19b5239ac3cde6a363a22e462f8
Felix Dango <felix.dango@reaze.com>
2021-10-09 12:26:18 +0200

    fix: test if secret key needs to be set in settings to work with pipeline

--- accesslog/settings.py
+++ accesslog/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-+!ik96zts$xx9pe-dgc2mdvchx3v@0dq-*&d8j$s404w7@lu-@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(env("DEBUG", default=0))
