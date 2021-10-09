cfc48f1b29519a04e67b47b6c37b2cc2436aa89b
Felix Dango <felix.dango@reaze.com>
2021-10-09 12:29:56 +0200

    fix: test if .env.dev works with github workflow

--- accesslog/settings.py
+++ accesslog/settings.py
@@ -23,14 +23,14 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env.dev'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+!ik96zts$xx9pe-dgc2mdvchx3v@0dq-*&d8j$s404w7@lu-@'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(env("DEBUG", default=0))
