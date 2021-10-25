f646296a3e3553e470f7204ee250f18ad2120bae
Felix Dango <felix.dango@reaze.com>
2021-10-25 16:11:54 +0200

    feat: add unit test to the ThreeDimensionalModel Upload
    relates to: #2

--- rest/tests.py
+++ rest/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import tempfile
 
-# Create your tests here.
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from print.models import *
+
+class ThreeDimensionalModelTests(APITestCase):
+
+    def setUp(self):
+        """
+        Create Test User to authenticate
+        """
+        User.objects.create_user('testuser')
+
+    def test_create_model(self):
+        """
+        Ensure we can upload a ThreeDimensionalModel.
+        """
+        client = APIClient()
+        user = User.objects.get(username='testuser')
+        client.force_authenticate(user=user)
+        file = SimpleUploadedFile("file.obj", b"file_content", content_type="application/octet-stream")
+        response = client.post(reverse('ThreeDimensionalModel-list'), {'File': file})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Test if there is actually one file uploaded
+        self.assertEqual(ThreeDimensionalModel.objects.count(), 1)
--- rest/views.py
+++ rest/views.py
@@ -1,13 +1,10 @@
 from rest_framework.response import Response
-from rest_framework import permissions, authentication
+from rest_framework import permissions
 from rest_framework.views import APIView
-
 from rest.serializers import *
 from print.models import *
 from django.db.models import Q
 from rest_framework import viewsets
-import json
-from django.http import HttpResponse
 import os
 
 class ThreeDimensionalModelViewSet(viewsets.ModelViewSet):
