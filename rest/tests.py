34663fc1d833b64b1c6c19f77ef83ef9a7dc9e9b
Felix Dango <felix.dango@reaze.com>
2021-10-25 16:52:49 +0200

    feat: add more tests to ThreeDimensionalFile Upload
    relates to: #2

--- rest/tests.py
+++ rest/tests.py
@@ -21,8 +21,20 @@ class ThreeDimensionalModelTests(APITestCase):
         client = APIClient()
         user = User.objects.get(username='testuser')
         client.force_authenticate(user=user)
-        file = SimpleUploadedFile("file.obj", b"file_content", content_type="application/octet-stream")
-        response = client.post(reverse('ThreeDimensionalModel-list'), {'File': file})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Test correct input response
+        correct_file = SimpleUploadedFile("file.obj", b"file_content", content_type="application/octet-stream")
+        correct = client.post(reverse('ThreeDimensionalModel-list'), {'File': correct_file})
+        self.assertEqual(correct.status_code, status.HTTP_200_OK)
         # Test if there is actually one file uploaded
         self.assertEqual(ThreeDimensionalModel.objects.count(), 1)
+
+        # Test incorrect file type input response
+        incorrect_file = SimpleUploadedFile("file.jpg", b"file_content", content_type="image/jpeg")
+        incorrect_type = client.post(reverse('ThreeDimensionalModel-list'), {'File': incorrect_file})
+        self.assertNotEqual(incorrect_type.status_code, status.HTTP_200_OK)
+        self.assertEqual(incorrect_type.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+        # Test null input, bad request
+        incorrect_request = client.post(reverse('ThreeDimensionalModel-list'), None)
+        self.assertEqual(incorrect_request.status_code, status.HTTP_400_BAD_REQUEST)
