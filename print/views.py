45a3a72bc871dd65f5a74bbb946f94af66c62448
Felix Dango <felix.dango@reaze.com>
2021-11-11 16:48:57 +0100

    feat: cleanup overhead while trying to figure out topic event, general cleanup
    relates to: #36

--- print/views.py
+++ print/views.py
@@ -6,83 +6,84 @@ import json
 from datetime import datetime
 from print.models import Machine, PrintJob, BedTemperatureHistory, ToolTemperatureHistory
 from django.utils import timezone
-def handle_msg(topic, message):
-    printer = topic.split("/")[2]
-    p_id = Machine.objects.get(Name=printer).id
-    m = message.decode("Utf-8")
 
-    # Check if topic is PrintDone
+def check_topic(topic):
+    temp = None
+    event = None
+    
     try:
-        done = topic.split("/")[4]
-        if done != "PrintDone":
-            done = None
+        event = topic.split("/")[4]
     except:
-        done = None
+        pass
 
-    # Check if topic is PrintStarted
-    try:
-        start = topic.split("/")[4]
-        if start != "PrintStarted":
-            start = None
-    except:
-        start = None
-
-    # Check if topic is Temperature and if tool or bed temp is sent
     try:
         temp = topic.split("/")[3]
-        toolbed = topic.split("/")[4]
-        if temp != "temperature":
-            temp = None
-            toolbed = None
     except:
-        toolbed = None
-        temp = None
+        pass
+        
+    if event == "PrintDone":
+        return event
+    elif event == "PrintStarted":
+        return event
+    elif event == "PrintStarted":
+        return event
+    elif temp == "temperature":
+        return temp
 
-    if start == "PrintStarted":
+
+def handle_msg(topic, message):
+    printer = topic.split("/")[2]
+    printer_id = Machine.objects.get(Name=printer).id
+    m = message.decode("Utf-8")
+
+    # Check event of topic
+    event = check_topic(topic)
+    
+    if event == "PrintStarted":
         # Event: Printer is done with PrintJob
         print("Print started")
-        # Check if Printjob already exists
+        # Check if PrintJob already exists
         try:
-            PrintJob.objects.get(Machine_id=p_id)
-            p_exists = True
-        except:
-            p_exists = False
-
-        try:
-            if p_exists and PrintJob.objects.get(Machine_id=p_id).State == 0:
-                PrintJob.objects.create(Start=timezone.now(), End=timezone.now(), GCode_id=None,State=1, Machine_id=p_id,User_id=1)
-                print("Printjob created")
-            elif not p_exists:
-                PrintJob.objects.create(Start=timezone.now(), End=timezone.now(), GCode_id=None,State=1, Machine_id=p_id,User_id=1)
-                print("Printjob created")
-        except Exception as e:
-            print(e)
-
-    elif done == "PrintDone":
-        # Event: Printer is done with PrintJob
-        print("Print Done")
-
-        try:
-            PrintJob.objects.get(Machine_id=p_id, State=1)
+            PrintJob.objects.get(Machine_id=printer_id, State=1)
             p_exists = True
         except:
             p_exists = False
 
         try:
             if p_exists:
-                pj = PrintJob.objects.get(Machine_id=p_id, State=1)
-                pj.State = 0
-                pj.save()
-                print("Printjob state = 0")
+                print("PrintJob already exists")
+            else:
+                PrintJob.objects.create(Start=timezone.now(), End=timezone.now(), GCode_id=None,State=1, Machine_id=printer_id,User_id=1)
+                print("PrintJob created")
         except Exception as e:
             print(e)
 
-    elif temp == "temperature":
-        # Temperature is sent
+    elif event == "PrintDone":
+        # Event: Printer is done with PrintJob
+        print("Print Done")
+
+        # Check if PrintJob exists
+        try:
+            PrintJob.objects.get(Machine_id=printer_id, State=1)
+            p_exists = True
+        except:
+            p_exists = False
+
+        try:
+            if p_exists:
+                pj = PrintJob.objects.get(Machine_id=printer_id, State=1)
+                pj.State = 0
+                pj.save()
+                print("PrintJob State = 0")
+        except Exception as e:
+            print(e)
+
+    elif event == "temperature":
+        # Event: Temperature is sent
         target = None
         actual = None
         timestamp = None
-
+        tool_bed = topic.split("/")[4]
         # Set Temperature Data
         try:
             timestamp = datetime.fromtimestamp(json.loads(m)["_timestamp"], tz=pytz.timezone('Europe/Berlin'))
@@ -93,23 +94,22 @@ def handle_msg(topic, message):
 
         # Check if Machine has related PrintJob
         try:
-            PrintJob.objects.get(Machine_id=p_id, State=1)
+            PrintJob.objects.get(Machine_id=printer_id, State=1)
             p_exists = True
         except:
             p_exists = False
 
         # If Temperature is bed info
         if p_exists:
-            if toolbed == "bed":
+            if tool_bed == "bed":
                 try:
-                    BedTemperatureHistory.objects.create(PrintJob_id=PrintJob.objects.get(Machine_id=p_id, State=1).id, Target=target, Actual=actual, TimeStamp=timestamp)
-                    print(timestamp)
+                    BedTemperatureHistory.objects.create(PrintJob_id=PrintJob.objects.get(Machine_id=printer_id, State=1).id, Target=target, Actual=actual, TimeStamp=timestamp)
                 except Exception as e:
                     print(e)
 
             # If Temperature is tool info
-            elif toolbed == "tool0":
+            elif tool_bed == "tool0":
                 try:
-                    ToolTemperatureHistory.objects.create(PrintJob_id=PrintJob.objects.get(Machine_id=p_id, State=1).id, Target=target, Actual=actual, TimeStamp=timestamp)
+                    ToolTemperatureHistory.objects.create(PrintJob_id=PrintJob.objects.get(Machine_id=printer_id, State=1).id, Target=target, Actual=actual, TimeStamp=timestamp)
                 except Exception as e:
                     print(e)
